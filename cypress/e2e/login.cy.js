fc9c714bb8016955835ecce4b50e8b54f12877fb
Wellington Santos <wellingtondssantos26@gmail.com>
2025-02-04 14:35:07 +0100

    Added selectors list for login spec

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -5,7 +5,8 @@ const selectorList = {
   passwordField: "[name='password']",
   loginButton: "[type='submit']",
   sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
-  wrongCrentialAlert: "[role='alert']"
+  dashboardGrid: ".orangehrm-dashboard-grid",
+  wrongCrendentialAlert: "[role='alert']"
 }
 
   it('Login - Sucess', () => {
@@ -14,13 +15,13 @@ const selectorList = {
    cy.get(selectorList.passwordField).type('admin123')
    cy.get(selectorList.loginButton).click()
    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-   cy.get(selectorList.sectionTitleTopBar).contains('Dashboard')
+   cy.get(selectorList.dashboardGrid)
   })
   it('Login - Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     cy.get(selectorList.usernameField).type('teste')
     cy.get(selectorList.passwordField).type('admin123')
     cy.get(selectorList.loginButton).click()
-    cy.get("role='alert']")
+    cy.get(selectorList.wrongCrendentialAlert)
    })
 })
