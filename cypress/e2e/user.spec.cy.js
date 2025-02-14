4b69ed2976aa357768d7176b9af52eec1a5a244e
Wellington Santos <wellingtondssantos26@gmail.com>
2025-02-14 14:03:20 +0100

    User update scenario added

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -9,7 +9,12 @@ const selectorList = {
   sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
   dashboardGrid: ".orangehrm-dashboard-grid",
   wrongCrendentialAlert: "[role='alert']",
-  myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]'
+  myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+  firstNameField: "[name='firstName']",
+  lastNameField: "[name='lastName']",
+  genericField: ".oxd-input--active",
+  dateCloseButton:".--close",
+  submitButton: "[type='submit']",
 }
 
 it.only('User Info Update - Sucess', () => {
@@ -20,7 +25,12 @@ it.only('User Info Update - Sucess', () => {
    cy.get(selectorList.loginButton).click()
    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
    cy.get(selectorList.dashboardGrid)
-   cy.get(selectorList.myInfoButton).click
+   cy.get(selectorList.myInfoButton).click()
+   cy.get(selectorList.firstNameField).clear().type('FirstNameTest')
+   cy.get(selectorList.lastNameField).clear().type('LastNameTest')
+   cy.get(selectorList.genericField).eq(3).clear().type('Employee')
+   cy.get(selectorList.submitButton).eq(0).click()
+   cy.get ('body').should('contain', 'Successfully Updated')
   })
 
 it('Login - Fail', () => {
